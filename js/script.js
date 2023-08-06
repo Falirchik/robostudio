2a251204606817b2e82c70ae2918e691f600e232
Разживина Анастасия <54889832+Falirchik@users.noreply.github.com>
2023-08-06 23:57:57 +0300

    .

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@ $(function() {
     let hederHeight = header.height(); // вычисляем высоту шапки
 
     $(window).scroll(function() {
-        if ($(this).scrollTop() >199) {
-            header.addClass('header_fixed');
-            $('body').css({
-                'paddingTop': hederHeight + 'px' // делаем отступ у body, равный высоте шапки
-            });
-        } else {
-            header.removeClass('header_fixed');
-            $('body').css({
-                'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
-            })
-        }
+        // if ($(this).scrollTop() >199) {
+        //     header.addClass('header_fixed');
+        //     $('body').css({
+        //         'paddingTop': hederHeight + 'px' // делаем отступ у body, равный высоте шапки
+        //     });
+        // } else {
+        //     header.removeClass('header_fixed');
+        //     $('body').css({
+        //         'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+        //     })
+        // }
 
         if ($(this).scrollTop() > 300) {
             header.css({
