a6e94ad92642a3310afb3a0385ab7b93bb3f0523
Разживина Анастасия <54889832+Falirchik@users.noreply.github.com>
2023-08-10 23:09:13 +0300

    update carusel

--- js/script.js
+++ js/script.js
@@ -15,3 +15,35 @@ $(function() {
 });
 
 
+const carousel_2 = document.querySelector('.carousel_2');
+const images = carousel_2.querySelectorAll('img');
+const prevButton = carousel_2.querySelector('.prev-button');
+const nextButton = carousel_2.querySelector('.next-button');
+let currentImageIndex = 0;
+
+prevButton.addEventListener('click', showPreviousImage);
+nextButton.addEventListener('click', showNextImage);
+
+function showPreviousImage() {
+  images[currentImageIndex].classList.remove('current');
+
+  if (currentImageIndex === 0) {
+    currentImageIndex = images.length - 1;
+  } else {
+    currentImageIndex--;
+  }
+
+  images[currentImageIndex].classList.add('current');
+}
+
+function showNextImage() {
+  images[currentImageIndex].classList.remove('current');
+
+  if (currentImageIndex === images.length - 1) {
+    currentImageIndex = 0;
+  } else {
+    currentImageIndex++;
+  }
+
+  images[currentImageIndex].classList.add('current');
+}
